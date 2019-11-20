740143a5fc7e9e5ad868005390127ee918bfe417
Sitharthan Vk <sitharthan.v@HCL.COM>
2019-11-20 19:57:53 +0530

    [MEAN Stack]Created: Angular8MeanstackAngularMaterial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { AngularMaterialModule } from './material.module';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+/* Angular 8 components */
 import { AddStudentComponent } from './components/add-student/add-student.component';
 import { EditStudentComponent } from './components/edit-student/edit-student.component';
 import { StudentsListComponent } from './components/students-list/students-list.component';
+
+/* Angular material */
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './material.module';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+/* Angular 8 http service */
+import { HttpClientModule } from '@angular/common/http';
+
+/* Angular 8 CRUD services */
+import { ApiService } from './shared/api.service';
+
+/* Reactive form services in Angular 8 */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -16,11 +31,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     StudentsListComponent
   ],
   imports: [
+    BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
+
 export class AppModule { }
---
+++ src/app/shared/student.ts
@@ -0,0 +1,12 @@
+export class Student {
+    // tslint:disable-next-line: variable-name
+    _id: string;
+    // tslint:disable-next-line: variable-name
+    student_name: string;
+    // tslint:disable-next-line: variable-name
+    student_email: string;
+    section: string;
+    subjects: Array<string>;
+    dob: Date;
+    gender: string;
+}
